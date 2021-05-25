4cc414ae6ad65d95ba54675cca76acfdd34187d1
vardanarora <vardaan1303@gmail.com>
2021-05-25 13:42:31 +0530

    modifidedboxfile

--- Box.js
+++ Box.js
@@ -1,6 +1,6 @@
 class Box extends BaseClass {
-  constructor(x, y, width, height){
-    super(x,y,width,height);
+  constructor(x, y, width, ){
+    super(x,y,width,200);
     this.image = loadImage("sprites/wood1.png");
   }
 
